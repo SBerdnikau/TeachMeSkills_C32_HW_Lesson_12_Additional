1e78ad9c136ea3a341338979c681a269e07f45e6
Sergey <berdnikausiarhei@gmail.com>
2024-11-14 12:09:55 +0300

    TCM-C32-HM-Lesson-12-Additional
    1. Task 1 was refreshed

--- src/com/teachmeskills/lesson_12_additional/task1/Runner.java
+++ src/com/teachmeskills/lesson_12_additional/task1/Runner.java
@@ -1,6 +1,10 @@
 package com.teachmeskills.lesson_12_additional.task1;
 
+import javax.xml.stream.events.Characters;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Пользователь вводит трехзначное число.
@@ -9,15 +13,24 @@ import java.util.Scanner;
  * как 3 + 4 + 9 = 16.
  */
 public class Runner {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Insert number: ");
+        System.out.print("Insert three-digit number: ");
         String digitNumber = scanner.nextLine();
-        String[] charactersArray = digitNumber.split("");
-        int result = 0;
-        for (String item : charactersArray) {
-            result += Integer.parseInt(item);
+
+        Pattern regexp = Pattern.compile("[0-9]{3}");
+        Matcher matcher = regexp.matcher(digitNumber);
+        if (matcher.matches()) {
+            char[] charactersArray = digitNumber.toCharArray();
+            int result = 0;
+            for (char item : charactersArray) {
+                 result += Character.getNumericValue(item);
+            }
+            System.out.println("Result = " + result);
+        } else {
+            System.out.println("Please! Insert only three-digit number! ");
         }
-        System.out.println("Result = " + result);
+
     }
 }
